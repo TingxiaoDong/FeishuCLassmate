15d6fef459a1e70b6ad2cc0a8c7ec99e06a66e58
XuYuan <1659786794@qq.com>
2026-05-14 01:53:49 +0800

    update

--- src/check_module.cjs
+++
@@ -1,6 +0,0 @@
-// Dynamic import ES module from CommonJS
-const { createRequire } = module;
-const require = createRequire(import.meta.url);
-
-const layout = require('./tools/data-layout.js');
-console.log('data-layout exports:', Object.keys(layout));
--- src/temp_tour_check.cjs
+++
@@ -1,9 +0,0 @@
-const { feishu_classmate_data_layout, feishu_classmate_temi_status } = require("./tools");
-(async () => {
-  const layout = await feishu_classmate_data_layout();
-  console.log("=== LAYOUT ===");
-  console.log(JSON.stringify(layout, null, 2));
-  const status = await feishu_classmate_temi_status();
-  console.log("=== TEMI STATUS ===");
-  console.log(JSON.stringify(status, null, 2));
-})();
